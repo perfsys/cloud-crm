f0a21918a3af9db36d43edac004c57ab2d4a1b04
eorlovsky <eorlovsky@perfsys.net>
2018-07-06 10:36:14 +0300

    unit tests fix

--- client/config/test.env.js
+++ client/config/test.env.js
@@ -1,7 +1,8 @@
-node_modules/.bin/cross-env API_BASE_URL=https://dnc2oskit7.execute-api.eu-west-1.amazonaws.com/dev npm run dev'use strict'
+'use strict'
 const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"'
+
 })
