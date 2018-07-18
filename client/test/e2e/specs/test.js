6451a3d5223c196a7777b204d05001725f760cb6
AShcherbakov <crusader34@gmail.com>
2018-07-18 19:49:22 +0300

    modified tests

--- client/test/e2e/specs/test.js
+++ client/test/e2e/specs/test.js
@@ -10,10 +10,9 @@ module.exports = {
 
     browser
       .url(devServer)
-      .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.containsText('h1', 'Welcome to Your Cloud CRM App')
+      .assert.elementCount('img', 0)
       .end()
   }
 }
