e020c5cae87515f1ef2587fc8ac393b80a70aa1a
lena <evasilkovich@perfsys.net>
2018-07-19 13:25:07 +0300

    update contact

--- client/src/api/contacts.js
+++ client/src/api/contacts.js
@@ -75,6 +75,32 @@ export default {
       linkedin_link: item.linkedin_link
     }
     )
+  },
+
+  /**
+   * Update Contact
+   *
+   * @param contact
+   * @returns {AxiosPromise<any> | * | void}
+   */
+  updateOne (item) {
+    return HTTP.put(`/contacts/${item.group_id}/${item.name}`, {
+        group_id: item.group_id,
+        name: item.name,
+        source_id: item.source_id,
+        first_name: item.first_name,
+        last_name: item.last_name,
+        type_id: item.type_id,
+        status_id: item.status_id,
+        company_name: item.company_name,
+        company_www: item.company_www,
+        country_code: item.country_code,
+        position: item.position,
+        facebook_link: item.facebook_link,
+        twitter_link: item.twitter_link,
+        linkedin_link: item.linkedin_link
+      }
+    )
   }
 
 }
