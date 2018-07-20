4501fc1762b677fee1454bd2c9edeed9bcb0a7c7
AShcherbakov <crusader34@gmail.com>
2018-07-20 18:25:30 +0300

    broke

--- client/test/unit/specs/HelloWorld.spec.js
+++ client/test/unit/specs/HelloWorld.spec.js
@@ -5,7 +5,7 @@ describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(HelloWorld)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Cloud CRM App')
+    expect(vm.$el.querySelector('1').textContent)
+      .toEqual('2')
   })
 })
