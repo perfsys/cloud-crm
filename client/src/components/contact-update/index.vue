d43dfcb7e03df8260c4fa7abe0e750ec99e32c32
lena <evasilkovich@perfsys.net>
2018-07-24 18:36:51 +0300

    buttons in right corner

--- client/src/components/contact-update/index.vue
+++ client/src/components/contact-update/index.vue
@@ -5,8 +5,6 @@
         <div class="md-title">{{contact.group_name}}  {{contact.name}}</div>
       </md-card-header>
 
-      <div class="md-layout">
-      <div class="md-layout-item">
       <md-card-content class="md-scrollbar " >
 
         <md-list class="md-double-line">
@@ -28,131 +26,136 @@
 
         <form novalidate @submit.prevent="onSubmit" >
 
-          <md-field>
-            <label for="status_id">Status</label>
-            <md-select v-model="contact.status_id" name="status_id" id="status_id">
+          <div class="md-layout">
+            <div class="md-layout-item">
 
-              <md-option v-for="status in statuses" :value="status.id" :key="status.id">{{ status.name }}
-              </md-option>
+              <md-field>
+                <label for="status_id">Status</label>
+                <md-select v-model="contact.status_id" name="status_id" id="status_id">
 
-            </md-select>
-          </md-field>
+                  <md-option v-for="status in statuses" :value="status.id" :key="status.id">{{ status.name }}
+                  </md-option>
 
-          <div class="md-layout-item md-small-size-100">
-            <md-radio v-model="contact.type_id" value="COLD">Cold</md-radio>
-            <md-radio v-model="contact.type_id" value="WARM">Warm</md-radio>
-            <md-radio v-model="contact.type_id" value="HOT">Hot</md-radio>
+                </md-select>
+              </md-field>
+
+              <div class="md-layout-item md-small-size-100">
+                <md-radio v-model="contact.type_id" value="COLD">Cold</md-radio>
+                <md-radio v-model="contact.type_id" value="WARM">Warm</md-radio>
+                <md-radio v-model="contact.type_id" value="HOT">Hot</md-radio>
+              </div>
+
+              <md-steppers md-vertical>
+
+                <md-step id="first" md-label="Person">
+
+                    <div class="md-layout-item md-small-size-100">
+                      <md-field>
+                      <label for="first_name">First Name</label>
+                      <md-input name="first_name" id="first_name" autocomplete="first_name" v-model="contact.first_name"/>
+                      </md-field>
+                    </div>
+
+                    <div class="md-layout-item md-small-size-100">
+                    <md-field>
+                    <label for="last_name">Last Name</label>
+                    <md-input name="last_name" id="last_name" autocomplete="last_name" v-model="contact.last_name"/>
+                    </md-field>
+                    </div>
+
+                    <div class="md-layout-item md-small-size-100">
+                    <md-field>
+                    <label for="position">Position</label>
+                    <md-input name="position" id="position" autocomplete="position" v-model="contact.position"/>
+                    </md-field>
+                    </div>
+
+                    <div class="md-layout-item md-small-size-100">
+                    <md-field>
+                      <label for="source_id">Source</label>
+                      <md-select v-model="contact.source_id" name="source_id" id="source_id">
+                        <md-option v-for="source in sources" :value="source.id" :key="source.id">{{ source.name }}
+                        </md-option>
+                      </md-select>
+                    </md-field>
+                    </div>
+
+                </md-step>
+
+                <md-step id="second" md-label="Company">
+
+                    <div class="md-layout-item md-small-size-100">
+                    <md-field>
+                    <label for="company_name">Company Name</label>
+                    <md-input name="company_name" id="company_name" autocomplete="company_name" v-model="contact.company_name"/>
+                    </md-field>
+                    </div>
+
+                    <div class="md-layout-item md-small-size-100">
+                      <md-field>
+                        <label for="company_www">Company www</label>
+                        <md-input name="company_www" id="company_www" autocomplete="company_www" v-model="contact.company_www"/>
+                      </md-field>
+                    </div>
+
+                    <md-field>
+                      <label for="country">Country</label>
+                      <md-select v-model="contact.country_code" name="country" id="country">
+
+                        <md-option v-for="country in countries" :value="country.abbreviation" :key="country.abbreviation">{{ country.country }}
+                        </md-option>
+
+                      </md-select>
+                    </md-field>
+
+                </md-step>
+                <md-step id="third" md-label="Links">
+                    <div class="md-layout-item md-small-size-100">
+                      <md-field>
+                        <label for="facebook_link">Facebook Link</label>
+                        <md-input name="facebook_link" id="facebook_link" autocomplete="facebook_link" v-model="contact.facebook_link"/>
+                      </md-field>
+                    </div>
+
+                    <div class="md-layout-item md-small-size-100">
+                      <md-field>
+                        <label for="twitter_link">Twitter Link</label>
+                        <md-input name="twitter_link" id="twitter_link" autocomplete="twitter_link" v-model="contact.twitter_link"/>
+                      </md-field>
+                    </div>
+
+                    <div class="md-layout-item md-small-size-100">
+                      <md-field>
+                        <label for="linkedin_link">Linkedin Link</label>
+                        <md-input name="linkedin_link" id="linkedin_link" autocomplete="linkedin_link" v-model="contact.linkedin_link"/>
+                      </md-field>
+                    </div>
+
+                </md-step>
+              </md-steppers>
+
+          </div>
+            <div class="md-layout-item"></div>
           </div>
 
-          <md-steppers md-vertical>
+          <md-dialog-actions>
+            <md-button class="md-primary" @click="onClose">Close</md-button>
+            <md-button class="md-primary" type="submit">Save</md-button>
+          </md-dialog-actions>
 
-            <md-step id="first" md-label="Person">
+          <md-dialog-alert
+            :md-active.sync="successSnackbar"
+            md-content="Contact was updated"
+            md-confirm-text="Close"/>
 
-                <div class="md-layout-item md-small-size-100">
-                  <md-field>
-                  <label for="first_name">First Name</label>
-                  <md-input name="first_name" id="first_name" autocomplete="first_name" v-model="contact.first_name"/>
-                  </md-field>
-                </div>
+          <md-dialog-alert
+            :md-active.sync="failedSnackbar"
+            :md-content="failedSnackbarReason"
+            md-confirm-text="Close"/>
 
-                <div class="md-layout-item md-small-size-100">
-                <md-field>
-                <label for="last_name">Last Name</label>
-                <md-input name="last_name" id="last_name" autocomplete="last_name" v-model="contact.last_name"/>
-                </md-field>
-                </div>
-
-                <div class="md-layout-item md-small-size-100">
-                <md-field>
-                <label for="position">Position</label>
-                <md-input name="position" id="position" autocomplete="position" v-model="contact.position"/>
-                </md-field>
-                </div>
-
-                <div class="md-layout-item md-small-size-100">
-                <md-field>
-                  <label for="source_id">Source</label>
-                  <md-select v-model="contact.source_id" name="source_id" id="source_id">
-                    <md-option v-for="source in sources" :value="source.id" :key="source.id">{{ source.name }}
-                    </md-option>
-                  </md-select>
-                </md-field>
-                </div>
-
-            </md-step>
-
-            <md-step id="second" md-label="Company">
-
-                <div class="md-layout-item md-small-size-100">
-                <md-field>
-                <label for="company_name">Company Name</label>
-                <md-input name="company_name" id="company_name" autocomplete="company_name" v-model="contact.company_name"/>
-                </md-field>
-                </div>
-
-                <div class="md-layout-item md-small-size-100">
-                  <md-field>
-                    <label for="company_www">Company www</label>
-                    <md-input name="company_www" id="company_www" autocomplete="company_www" v-model="contact.company_www"/>
-                  </md-field>
-                </div>
-
-                <md-field>
-                  <label for="country">Country</label>
-                  <md-select v-model="contact.country_code" name="country" id="country">
-
-                    <md-option v-for="country in countries" :value="country.abbreviation" :key="country.abbreviation">{{ country.country }}
-                    </md-option>
-
-                  </md-select>
-                </md-field>
-
-            </md-step>
-            <md-step id="third" md-label="Links">
-                <div class="md-layout-item md-small-size-100">
-                  <md-field>
-                    <label for="facebook_link">Facebook Link</label>
-                    <md-input name="facebook_link" id="facebook_link" autocomplete="facebook_link" v-model="contact.facebook_link"/>
-                  </md-field>
-                </div>
-
-                <div class="md-layout-item md-small-size-100">
-                  <md-field>
-                    <label for="twitter_link">Twitter Link</label>
-                    <md-input name="twitter_link" id="twitter_link" autocomplete="twitter_link" v-model="contact.twitter_link"/>
-                  </md-field>
-                </div>
-
-                <div class="md-layout-item md-small-size-100">
-                  <md-field>
-                    <label for="linkedin_link">Linkedin Link</label>
-                    <md-input name="linkedin_link" id="linkedin_link" autocomplete="linkedin_link" v-model="contact.linkedin_link"/>
-                  </md-field>
-                </div>
-
-            </md-step>
-          </md-steppers>
-
-        <md-dialog-actions>
-          <md-button class="md-primary" @click="onClose">Close</md-button>
-          <md-button class="md-primary" type="submit">Save</md-button>
-        </md-dialog-actions>
-
-        <md-dialog-alert
-          :md-active.sync="successSnackbar"
-          md-content="Contact was updated"
-          md-confirm-text="Close"/>
-
-        <md-dialog-alert
-          :md-active.sync="failedSnackbar"
-          :md-content="failedSnackbarReason"
-          md-confirm-text="Close"/>
-
-      </form>
+        </form>
       </md-card-content>
-      </div>
-      <div class="md-layout-item"></div>
-      </div>
+
     </md-card>
 
   </div>
