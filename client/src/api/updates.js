0cb5f5fb8a302d12d0c43ba3e0f527b022e33535
evasilkovich <vev77777@gmail.com>
2018-09-03 17:29:30 +0300

    fix URI

--- client/src/api/updates.js
+++ client/src/api/updates.js
@@ -4,21 +4,21 @@ export default {
 
   getAllByContact (group, name) {
     return new Promise((resolve, reject) => {
-      HTTP.get(`/contacts/group/${group}/name/${name}/updates`)
+      HTTP.get(`/contacts/${group}/${name}/updates`)
         .then(response => resolve(response.data), reject)
     })
   },
 
   getOneByContact (item) {
     return new Promise((resolve, reject) => {
-      HTTP.get(`/contacts/group/${item.group}/name/${item.name}/updates/${item.id}`)
+      HTTP.get(`/contacts/${item.group}/${item.name}/updates/${item.id}`)
         .then(response => resolve(response.data), reject)
     })
   },
 
   saveOneByContact (group, name, text) {
     return new Promise((resolve, reject) => {
-      HTTP.post(`/contacts/group/${group}/name/${name}/updates`, {
+      HTTP.post(`/contacts/${group}/${name}/updates`, {
         text: text
       }).then(response => resolve(response.data), reject)
     })
@@ -26,14 +26,14 @@ export default {
 
   editOneByContact (update) {
     return new Promise((resolve, reject) => {
-      HTTP.put(`/contacts/group/${update.group}/name/${update.name}/updates/${update.id}`, {
+      HTTP.put(`/contacts/${update.group}/${update.name}/updates/${update.id}`, {
         text: update.text
       }).then(response => resolve(response.data), reject)
     })
   },
 
   deleteOneByContact (item) {
-    return HTTP.delete(`/contacts/group/${item.group}/name/${item.name}/updates/${item.id}`)
+    return HTTP.delete(`/contacts/${item.group}/${item.name}/updates/${item.id}`)
   }
 
 }
