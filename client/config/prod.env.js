fb62618de8f79dc14bac2a72cf7bdfbaedac0bf1
evasilkovich <vev77777@gmail.com>
2019-03-26 17:47:13 +0200

    for prod

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: `'${process.env.API_BASE_URL}'`
+  API_BASE_URL: `'${process.env.API_BASE_URL}'`,
+  S3_UPDATES_DATA_BUCKET: `'perfsys-cloud-crm-updates-data'`
 }
