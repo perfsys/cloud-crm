64f8b40bef007a53f64b04dbd3a8e8cc4a443a79
evasilkovich <vev77777@gmail.com>
2019-04-03 15:09:52 +0300

    fix lint

--- server/deployUtils.js
+++ server/deployUtils.js
@@ -1,12 +1,10 @@
 const getCleanString = (serverless) => {
-
   let stage = (serverless.config.serverless.processedInput.options.stage) ? serverless.config.serverless.processedInput.options.stage : 'dev'
 
   serverless.cli.consoleLog(stage)
-  serverless.cli.consoleLog(stage.replace(/[^\w+]/gim,''))
+  serverless.cli.consoleLog(stage.replace(/[^\w+]/gim, ''))
 
-  return stage.replace(/[^\w+]/gim,'')
+  return stage.replace(/[^\w+]/gim, '')
 }
 
 module.exports = {getCleanString}
-
