afb2af51ac8190ba2e1a8b0a213bbc2e6fa6ed1f
evasilkovich <vev77777@gmail.com>
2019-04-04 14:08:51 +0300

    to throw error if stage length is too big

--- server/deployUtils.js
+++ server/deployUtils.js
@@ -4,7 +4,13 @@ const getCleanString = (serverless) => {
   serverless.cli.consoleLog(stage)
   serverless.cli.consoleLog(stage.replace(/[^\w+]/gim, ''))
 
-  return stage.replace(/[^\w+]/gim, '')
+  let newName = stage.replace(/[^\w+]/gim, '')
+
+  if(newName.length > 7){
+    serverless.cli.consoleLog(`Stage length is ${newName.length}`)
+    throw new Error('Stage length is bigger 7')
+  }
+  return newName
 }
 
 module.exports = {getCleanString}
